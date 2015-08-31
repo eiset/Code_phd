f73038e13f5217ddd5c12fd57236b6001ee0635d
Andreas Halgreen Eiset <eiset@hotmail.com>
2015-08-31 21:33:52 +0200

    Update sample_size.R

--- sample_size.R
+++ sample_size.R
@@ -11,6 +11,9 @@ e = 0.05  # margin of error
 (n = qnorm(1-0.05 / 2)^2 * p * (1-p) / e^2)
 #[1] 195.9144
 
+# Normal approximation is assumed. To formally check this n and n * (1-p) must both be greater than 5.
+# n = 195.9144 and n * (1 - 0.15) = 166.5272.
+
 
 # 2. Sample size needed for detecting a prevalence of 20 in the Middle Eastern
 # refugee population.
@@ -22,9 +25,3 @@ e = 0.05  # margin of error
 
 
 # The same calculations are the basis for the sample size calculation for study c)
-
-# Normal approximation is assumed. To formally check this n and n * (1-p) must both be greater than 5.
-# n = 185.0047 and n * (1-0.14) = 159.104.
-
-
-
